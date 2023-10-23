780a94e97c4454c2fd77a77e4197a75926652d61
DYLKO Yury <k3work@gmail.com>
2023-10-23 19:39:50 +0300

    fix: correct variable substitution in pre_prelease.sh

--- pre_release.sh
+++ pre_release.sh
@@ -1,8 +1,8 @@
 set -eu
 
-NEW_DOC_VER="**Latest Version: $NEW**"
-NEW_BUILD_VER="  val releaseVersion = \"$NEW\""
+NEW_DOC_VER="**Latest Version: $1**"
+NEW_BUILD_VER="  val releaseVersion = \"$1\""
 
-echo "Updating versions to $NEW ..."
+echo "Updating versions to $1 ..."
 sed -i '/**Latest Version/c\'"$NEW_DOC_VER" docs/index.md
 sed -i '/  val releaseVersion/c\'"$NEW_BUILD_VER" project/Version.scala
