b3f08ea7f1cb7ea36a4d2588d90b9f8597de1747
Yury Dylko <62417327+gabb1er@users.noreply.github.com>
2023-10-25 15:47:18 +0300

    feat: add Fibonacci number function (#2)
    Adding fibonacci number function to Greeting trait. Function is tail
    recursive and returns BigInt number. Also added test for this function.

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,5 +1,7 @@
 package example
 
+import scala.annotation.tailrec
+
 object Hello extends Greeting with App {
   println(greeting)
 
@@ -11,8 +13,32 @@ object Hello extends Greeting with App {
     * @return Sum of first and second number
     */
   def sum(a: Int, b: Int): Int = a + b
+
+  val n = 10
+  println(s"Computed $n-th Fibonacci number: ${fibonacci(n)}")
+  val nn = 255
+  println(s"Computed $nn-th Fibonacci number: ${fibonacci(nn)}")
 }
 
 trait Greeting {
   lazy val greeting: String = "hello"
+
+  /**
+    * Computes n-th Fibonacci number
+    *
+    * @param n Fibonacci number to compute
+    * @return n-th Fibonacci number
+    */
+  def fibonacci(n: Int): BigInt = {
+    @tailrec
+    def loop(n: Int, acc1: BigInt, acc2: BigInt): BigInt =
+      n match {
+        case 0 => acc1
+        case 1 => acc2
+        case _ => loop(n - 1, acc2, acc1 + acc2)
+      }
+    
+    if (n < 0) throw new IllegalArgumentException("Cannot compute negative Fibonacci number")
+    else loop(n, 0, 1)
+  }
 }
